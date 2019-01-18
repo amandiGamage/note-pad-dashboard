e3b9d6e8331214cd0841db4f6164b47d22004beb
harindu liyanage <harindu.sul@gmail.com>
2019-01-18 14:29:04 +0530

    add router for applicaton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
 import { NoteCreateComponent } from './note-create/note-create.component';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {Routes} from '@angular/router';
+import { NoteEditComponent } from './note-edit/note-edit.component';
+const appRoutes: Routes = [
+  {
+    path: 'notes-create',
+    component: NoteCreateComponent,
+    data: { title: 'Note Create' }
+  },
+  {
+    path: 'notes-edit/:id',
+    component: NoteEditComponent,
+    data: { title: 'Note Edit' }
+  },
+  /*{ path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }*/
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NoteCreateComponent
+    NoteCreateComponent,
+    NoteEditComponent
   ],
   imports: [
     AlertModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
