be05a8f72e2bd118aaeebf381b94fd672f90765b
harindu liyanage <harindu.sul@gmail.com>
2019-01-18 15:58:24 +0530

    create a note service

--- src/app/Services/note.service.ts
+++ src/app/Services/note.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
+const endpoint = 'http://localhost:8080/api/inshap.com/v1/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNotes(): Observable<any> {
+    return this.http.get(endpoint + 'notes').pipe(
+      map(this.extractData));
+  }
+
+  getNote(id): Observable<any> {
+    return this.http.get(endpoint + 'notes/' + id).pipe(
+      map(this.extractData));
+  }
+
+  addNote (note): Observable<any> {
+    console.log(note);
+    return this.http.post<any>(endpoint + 'notes', JSON.stringify(note), httpOptions).pipe(
+      tap((product) => console.log(`added Notes w/ id=${note.id}`)),
+      catchError(this.handleError<any>('addNote'))
+    );
+  }
+
+  updateNote (id, note): Observable<any> {
+    return this.http.put(endpoint + 'notes/' + id, JSON.stringify(note), httpOptions).pipe(
+      tap(_ => console.log(`updated notes id=${id}`)),
+      catchError(this.handleError<any>('updateNote'))
+    );
+  }
+
+  deleteNote (id): Observable<any> {
+    return this.http.delete<any>(endpoint + 'notes/' + id, httpOptions).pipe(
+      tap(_ => console.log(`deleted note id=${id}`)),
+      catchError(this.handleError<any>('deleteNote'))
+    );
+  }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
 }
